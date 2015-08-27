1615f4c98428217588c032d901ada63be1592cbc
Patrick Hoolboom <patrick@stackstorm.com>
2015-08-27 13:04:40 -0700

    Make port an integer

--- manifests/params.pp
+++ manifests/params.pp
@@ -40,7 +40,7 @@ class uchiwa::params {
                           }]
 
   $host            =     '0.0.0.0'
-  $port            =     '3000'
+  $port            =     3000
   $user            =     ''
   $pass            =     ''
   $refresh         =     '5'
